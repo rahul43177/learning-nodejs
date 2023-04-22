3a6e70a235acc9691c024010eab350139d603292
Rahul <rahulmishra4317@gmial.com>
2023-04-22 17:34:09 +0530

    learnt about async

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const fs = require('fs')
-const path = require('path')
-const dirPath = path.join(__dirname,'crud')
-const filePath = `${dirPath}/apple.txt`
-fs.writeFileSync(filePath,'THis is a simple text')
-// ye smaajh nahi aaya 
+console.log("start.exe...")
+console.log("logic.exe...")
+console.log("complete.exe")
+
+// asynchronous programming language means it does not wait the previous code to execute and then go to next code 
+// it does not wait 
+// it just executes without following an order and hence it is faster 
+/* js and nodeJs is an asynchronous programming language */
+let a = 10 
+let b = 0 
+
+setTimeout(()=>{
+    b=20
+
+},2000)
+console.log(a+b)
+//here it will not print 30 , rather it will print 10 becuase it is an asynchronous language and it is a problem with async language 
