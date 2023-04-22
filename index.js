c0edc01b6c5d8d932c9c6842bf24c5a7e7fb5242
Rahul <rahulmishra4317@gmial.com>
2023-04-22 17:42:06 +0530

    learnt about handling data

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-console.log("start.exe...")
-console.log("logic.exe...")
-console.log("complete.exe")
-
-// asynchronous programming language means it does not wait the previous code to execute and then go to next code 
-// it does not wait 
-// it just executes without following an order and hence it is faster 
-/* js and nodeJs is an asynchronous programming language */
 let a = 10 
 let b = 0 
 
-setTimeout(()=>{
-    b=20
 
-},2000)
-console.log(a+b)
-//here it will not print 30 , rather it will print 10 becuase it is an asynchronous language and it is a problem with async language 
+let waitingData = new Promise((resolve,reject)=> {
+    setTimeout(()=>{
+        resolve(30)   
+    })
+})
+
+// console.log(a+b)
+waitingData.then((data)=>{
+    b = data
+    console.log(a+b)
+})
