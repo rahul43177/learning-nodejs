8530300a647ab7d1ab2f79f47b0f80528e9f19d9
Rahul <rahulmishra4317@gmial.com>
2023-04-22 17:53:35 +0530

    learnt about working of nodes and it was interesting

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-let a = 10 
-let b = 0 
+/**how node js works 
+ * Call stack ---> tells which code to execute first ,whenever execution of the function is over the function leaves call stack 
+ */
+console.log("Starting up ")
+
+setTimeout(()=>{
+    console.log("2 seconds log")
+},2000)
+
+setTimeout(()=>{
+    console.log("0 seconds log")
+},0)
+
+console.log("finishing up ")
+
+/* order -> 
+stating up 
+finishing up 
+0 log 
+2 log    */
 
 
-let waitingData = new Promise((resolve,reject)=> {
-    setTimeout(()=>{
-        resolve(30)   
-    })
-})
-
-// console.log(a+b)
-waitingData.then((data)=>{
-    b = data
-    console.log(a+b)
-})
+//this setTIme is from C++ and hence all those things which node takes from C++ are gone to Node APIs 
