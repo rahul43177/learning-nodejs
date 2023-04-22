47801f6e3ab59165ebf8808d5345fc37119cf1a9
Rahul <rahulmishra4317@gmial.com>
2023-04-22 16:38:35 +0530

    created a simple server

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-// node-mon package - time saving module
-//if I change anything we dont need to change the code or run again and again  
-// to not run and stop server again and again - we use nodemon 
-// install nodemon => npm i nodemon -g (here g is for globally)
-// tu run nodemon --> nodemon index.js
-console.log("to run nodemon => nodemon index.js")
-// node js is a async language and node js is single threaded langugage 
-// sync => pehle first chalegi fir second chalegi 
-// async => no order , they work together ,it doesnt wait 
-// that is why js or node js is fast 
+// making a simple API 
+const http = require('http')
+http.createServer((req,res)=>{
+    res.writeHead(200,{'content':'application\json'})
+    res.write({name : 'Rahul Mishra',email : 'rahulmisrha@gmail.com'})
+    res.end()
+}).listen(5000)
+// 
