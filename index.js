8c0f0172c1199cdd450af4774d709a6010557cf1
Rahul <rahulmishra4317@gmial.com>
2023-04-23 09:24:41 +0530

    HTML pages using nodejs banaye

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-//write html inside node js and we can do it 
-// we can give anchor tag and any HTML from here 
 const express = require('express')
-const app = express() //we make it executable 
-app.get('',(req,res)=>{
-    // console.log("data sent by browser",req.query.name)
-    res.send(`
-    <h1> Welcome , to home Page </h1>  <a href="/about" >GO to about  </a>
-    `)
-})
-
-app.get('/about',(req,res)=>{
-    res.send(`
-    <input type = "text" placeholder = "username" /> 
-
-    <button> Click me  </button>  
-    <a href="/" > Go to home page </a>   
-    `)
-})
+const path = require('path')
 
 
-app.listen(5000) //app.listen is used for making port
+const app = express()
+const publicPath=path.join(__dirname,'public')
+
+// app.use -> it is a middleware 
+app.use(express.static(publicPath))
+
+app.listen(5000)
